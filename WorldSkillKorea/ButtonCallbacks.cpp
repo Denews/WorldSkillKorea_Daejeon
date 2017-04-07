eec307b6ea14070d04d0598416558c274fa492a1
user <user@SW-05>
2017-04-07 13:59:24 +0900

    배경dds파일추가

--- WorldSkillKorea/ButtonCallbacks.cpp
+++ WorldSkillKorea/ButtonCallbacks.cpp
@@ -8,30 +8,36 @@
 
 void StartCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::curLevel = new Stage1(GameState::graphics->getDevice());
 }
 
 void IntroduceCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::curLevel = new Introduce(GameState::graphics->getDevice());
 }
 
 void HowToplayCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::curLevel = new HowToPlay(GameState::graphics->getDevice());
 }
 
 void RankingCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::curLevel = new Ranking(GameState::graphics->getDevice());
 }
 
 void CreditCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::curLevel = new Credit(GameState::graphics->getDevice());
 }
 
 void QuitCallback::onSelected()
 {
+	delete GameState::curLevel;
 	GameState::gameShouldRunning = false;
 }
