a7829bbbc1a58ea3bbe872a88ee52b59638102f1
user6 <user@SW-06>
2017-04-07 10:50:16 +0900

    버튼 대리로 넣어줌 존나 귀찮아

--- WorldSkillKorea/Submarine.h
+++ WorldSkillKorea/Submarine.h
@@ -6,5 +6,12 @@ class Submarine : public GameObject
 public:
 	Submarine() = delete;
 	Submarine(Submarine&) = delete;
-	Submarine(ID3D11Device* device, Texture* texture, )
+	Submarine(ID3D11Device* device, Texture* texture);
+	virtual ~Submarine();
+
+	virtual void update(float deltaTime) override;
+
+	virtual XMFLOAT3 getPosition() const override;
+private:
+
 };
