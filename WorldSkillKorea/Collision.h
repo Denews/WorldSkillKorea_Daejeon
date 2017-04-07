0af0c351903630e7bf631bf4809f5d7e6d110fd5
user <user@SW-05>
2017-04-07 15:06:31 +0900

    Collision

--- WorldSkillKorea/Collision.h
+++ WorldSkillKorea/Collision.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <D3D11.h>
+#include <xnamath.h>
+
+class Collision {
+public :
+	static bool CircleCollision(XMFLOAT2 Spot1, XMFLOAT2 Spot2, float r1, float r2);
+	static bool SquareCollision(XMFLOAT2 Spot1, XMFLOAT2 Spot2, XMFLOAT2 Spot3, XMFLOAT2 Spot4);
+	static bool CircleSquareCollision(XMFLOAT2 Spot1, XMFLOAT2 Spot2, XMFLOAT2 Circle, float r);
+};
